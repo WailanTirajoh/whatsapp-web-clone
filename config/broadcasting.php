47f1787f133aee6a88a5278f35b62f2611ae118a
Wailan Tirajoh <wailantirajoh@gmail.com>
2022-07-10 14:16:41 +0700

    chore: ssl for websocket

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -47,23 +47,39 @@ return [
         //     ],
         // ],
 
-        // Non SSL
+        // // Non SSL
+        // 'pusher' => [
+        //     'driver' => 'pusher',
+        //     'key' => env('PUSHER_APP_KEY'),
+        //     'secret' => env('PUSHER_APP_SECRET'),
+        //     'app_id' => env('PUSHER_APP_ID'),
+        //     'options' => [
+        //         'cluster' => env('PUSHER_APP_CLUSTER'),
+        //         'useTLS' => false,
+        //         'encrypted' => false,
+        //         'host' => env('PUSHER_HOST'),
+        //         'port' => env('PUSHER_PORT'),
+        //         'scheme' => env('PUSHER_SCHEME'),
+        //     ],
+        // ],
+
+        // SSL
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'useTLS' => false,
                 'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
                 'encrypted' => true,
                 'host' => env('PUSHER_HOST'),
                 'port' => env('PUSHER_PORT'),
                 'scheme' => env('PUSHER_SCHEME'),
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ],
+            ],
+            'client_options' => [
+                'verify' => false
+                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
         ],
 
